d4e9a11ed76d24cc4ce964d558e8b22dd25ca75b
Amitsen11 <89931804+Amitsen11@users.noreply.github.com>
2023-05-02 16:12:31 +0600

    cors installed

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const cors = require('cors')
+const app = express()
+const port = process.env.port || 5000;
+
+const chefList = require('./data/chefList.json')
+
+
+app.use(cors());
+
+
+app.get('/',(req, res)=>{
+    // res.json({ })
+    res.send('chef recipe is running')
+})
+
+app.get('/chefList', (req, res) => {
+    res.send(chefList)
+})
+
+app.listen(port, () => {
+    console.log(`chef recipe is running on Port:${port}`)
+})
